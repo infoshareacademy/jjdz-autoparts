889878487cffcf0a976c1b0be72bffd31baceb78
daniel-kepczynski <danielkepczynski@gmail.com>
2016-02-17 19:18:40 +0100

    Czytnik QR

---
+++ src/main/java/javatar/service/AztecCode.java
@@ -0,0 +1,56 @@
+package javatar.service;
+
+import com.google.zxing.*;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class AztecCode {
+    public static String readQRCode(String fileName) {
+        File file = new File(fileName);
+        BufferedImage image = null;
+        BinaryBitmap bitmap = null;
+        Result result = null;
+
+        try {
+            image = ImageIO.read(file);
+            int[] pixels = image.getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
+            RGBLuminanceSource source = new RGBLuminanceSource(image.getWidth(), image.getHeight(), pixels);
+            bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        if (bitmap == null)
+            return null;
+
+        QRCodeReader reader = new QRCodeReader();
+        try {
+            result = reader.decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ChecksumException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (FormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) throws IOException, NotFoundException {
+        String filePath = "aztec.png";
+        System.out.println(readQRCode(filePath));
+    }
+}
