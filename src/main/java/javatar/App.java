e7ab1501405209a911b5ec1dedeeb03b0431ed7f
AnetaK <karbowska.aneta@gmail.com>
2016-02-22 22:40:08 +0100

    JsonParser

--- src/main/java/javatar/App.java
+++ src/main/java/javatar/App.java
@@ -5,9 +5,6 @@ import javatar.service.JsonParserModels;
 
 import java.util.Collection;
 
-/**
- * Created by aneta on 13.02.16.
- */
 public class App {
 
 
---
+++ src/main/java/javatar/model/Response.java
@@ -0,0 +1,13 @@
+package javatar.model;
+
+
+import javatar.service.JsonParserModels;
+
+import java.util.Map;
+
+
+public class Response {
+    private Map<String, JsonParserModels> descriptor;
+}
+
+
