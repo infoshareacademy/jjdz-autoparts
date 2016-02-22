e76044c377efc0d8eb8959be383bd798fa761607
AnetaK <karbowska.aneta@gmail.com>
2016-02-22 20:14:03 +0100

    JsonParser

--- src/main/java/javatar/service/JsonParserModels.java
+++ src/main/java/javatar/service/JsonParserModels.java
@@ -2,12 +2,16 @@ package javatar.service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+
+import static org.apache.commons.lang3.builder.ToStringBuilder.*;
 
 
 public class JsonParserModels {
@@ -20,7 +24,7 @@ public class JsonParserModels {
         Path path = Paths.get(inFile);
         try (Reader reader = new InputStreamReader(Files.newInputStream(path))) {
             Object p = gson.fromJson(reader, outputClass);
-            System.out.println(p.toString());
+            System.out.println(reflectionToString(p, ToStringStyle.MULTI_LINE_STYLE));
        }
 
 
