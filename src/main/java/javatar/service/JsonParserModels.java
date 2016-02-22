c045f793288ad56af867ea7ac1074f3ac64439a1
AnetaK <karbowska.aneta@gmail.com>
2016-02-22 20:07:34 +0100

    JsonParser

--- src/main/java/javatar/service/JsonParserModels.java
+++ src/main/java/javatar/service/JsonParserModels.java
@@ -2,36 +2,29 @@ package javatar.service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
-import javatar.model.Models;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.nio.file.Files;
-import java.util.*;
-
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collection;
-import java.util.TreeSet;
+import java.nio.file.StandardOpenOption;
 
-public class JsonParserModels{
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        final Gson gson = new GsonBuilder().create();
-        Path path = Paths.get(args[0]);
+
+public class JsonParserModels {
+    public static void main(String[] args) throws ClassNotFoundException, IOException {
+        String inFile = args[0];
         String className = args[1];
+        Gson gson = new GsonBuilder().create();
 
-        final Class<?> outputClass = Class.forName(className);
-        try(
-            Reader reader = new InputStreamReader(Files.newInputStream(path))){
-                Object p = gson.fromJson(reader, outputClass);
-            }
+        Class<?> outputClass = Class.forName(className);
+        Path path = Paths.get(inFile);
+        try (Reader reader = new InputStreamReader(Files.newInputStream(path))) {
+            Object p = gson.fromJson(reader, outputClass);
+            System.out.println(p.toString());
+       }
 
 
 
     }
 
 }
-
-
