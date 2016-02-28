98a798caa078167eeca1f520d66c22fc790ebec0
michal.cymerys <michal.cymerys@gmail.com>
2016-02-28 18:08:27 +0100

    dodanie tymczasowej identyfikacji kategorii części

--- src/main/java/javatar/model/Autopart.java
+++ src/main/java/javatar/model/Autopart.java
@@ -1,22 +1,33 @@
 package javatar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Autopart {
-    private AutopartCategoryList autopartCategoryList;
+    private List<Category> categoryList;
     private String partName;
     private String partId;
 
-    public Autopart(AutopartCategoryList autopartCategoryList, String partName, String partId) {
-        this.autopartCategoryList = autopartCategoryList;
+    public Autopart() {
+        this.categoryList = new ArrayList<Category>();
+    }
+
+    public Autopart(List<Category> categoryList, String partName, String partId) {
+        this.categoryList = categoryList;
         this.partName = partName;
         this.partId = partId;
     }
 
-    public AutopartCategoryList getAutopartCategoryList() {
-        return autopartCategoryList;
+    public void addCategoryToList (Category category){
+        this.categoryList.add(category);
     }
 
-    public void setAutopartCategoryList(AutopartCategoryList autopartCategoryList) {
-        this.autopartCategoryList = autopartCategoryList;
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
+
+    public void setCategoryList(List<Category> categoryList) {
+        this.categoryList = categoryList;
     }
 
     public String getPartName() {
