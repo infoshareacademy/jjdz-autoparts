fadd4a554e67de95e1ba2c1cc48aa89c198a4a69
AnetaK <karbowska.aneta@gmail.com>
2016-03-05 15:00:21 +0100

    Pliki robocze

---
+++ src/main/java/javatar/service/CarIdentification.java
@@ -0,0 +1,11 @@
+package javatar.service;
+
+public class CarIdentification {
+    private String FirstStep {
+
+    }
+
+
+
+    System.out.println("Wybierz model z listy i wprowadź przyporządkowany mu numer:\r\n1. OPEL");
+}
---
+++ src/test/java/javatar/service/CarIdentificationTest.java
@@ -0,0 +1,8 @@
+package javatar.service;
+
+import static org.junit.Assert.*;
+
+
+public class CarIdentificationTest {
+
+}
