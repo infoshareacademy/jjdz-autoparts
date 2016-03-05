10f3ce2f5eb33a55acd6e2915b960c64743b633e
AnetaK <karbowska.aneta@gmail.com>
2016-03-05 17:43:04 +0100

    CarIdentifications - użycie JsonParsera

--- src/main/java/javatar/service/CarIdentification.java
+++ src/main/java/javatar/service/CarIdentification.java
@@ -1,11 +1,45 @@
 package javatar.service;
 
-public class CarIdentification {
-    private String FirstStep {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class CarIdentification {
+
+    private String FirstStep () {
+        return "";
+    }
+
+
+    public String FindingCarManagement() throws IOException {
+        String brand = new String();
+        String mainPath = "src/main/java/javatar/reasources/";
+        System.out.println("Wybierz markę z listy i wprowadź przyporządkowany jej numer:\r\n1. OPEL");
+        //String readBuffer = System.in.toString();
+        Scanner scanner = new Scanner(System.in);
+        String modelNumber = scanner.nextLine();
+
+        switch(modelNumber){
+            case "1": brand = "OPEL"; break;
+            default: brand = "";
+        }
+
+        JsonParserModels brandFileName = new JsonParserModels();
+        String path = mainPath +"v2.json";
+        String brandFileNameOut = brandFileName.searchCarId(brand,path);
+        System.out.println(brandFileNameOut);
+
+        return brandFileNameOut;
     }
 
 
 
-    System.out.println("Wybierz model z listy i wprowadź przyporządkowany mu numer:\r\n1. OPEL");
 }
+
+
+
+
+
+
+
+
