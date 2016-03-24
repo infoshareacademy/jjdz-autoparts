3b5ccd25f8a5e8521812bd645fd3d88edb66402b
AnetaK <karbowska.aneta@gmail.com>
2016-03-24 22:00:09 +0100

    allegro

--- src/test/java/javatar/service/AllegroCategoryFinderTest.java
+++ src/test/java/javatar/service/AllegroCategoryFinderTest.java
@@ -15,25 +15,45 @@ import static org.junit.Assert.*;
 public class AllegroCategoryFinderTest {
 
     @Test
-    public void testMatch2Categories() throws Exception {
+    public void testMatch3Categories() throws Exception {
 
         //given
         Autopart autopart = new Autopart();
         AutopartCategory categoryListElement = new AutopartCategory();
         categoryListElement.setName("Chłodzenie silnika");
+        autopart.addCategoryToList(categoryListElement);
         AutopartCategory categoryListElement2 = new AutopartCategory();
         categoryListElement2.setName("Chłodnice");
-        autopart.addCategoryToList(categoryListElement);
         autopart.addCategoryToList(categoryListElement2);
-
+        AutopartCategory categoryListElement3 = new AutopartCategory();
+        categoryListElement3.setName("Chłodnice oleju");
+        autopart.addCategoryToList(categoryListElement3);
 
         //when
         AllegroCategoryFinder allegroCategoryFinder = new AllegroCategoryFinder();
         String s = allegroCategoryFinder.MatchCategories(autopart);
 
         //then
-        assertThat(s, is(equalTo(" -> Chłodzenie silnika -> Chłodnice")));
+        assertThat(s, is(equalTo(" -> Chłodzenie silnika -> Chłodnice -> Chłodnice oleju")));
     }
 
+    @Test
+    public void testMatch2Categories() throws Exception {
 
+        //given
+        Autopart autopart = new Autopart();
+        AutopartCategory categoryListElement = new AutopartCategory();
+        categoryListElement.setName("Części karoserii");
+        autopart.addCategoryToList(categoryListElement);
+        AutopartCategory categoryListElement2 = new AutopartCategory();
+        categoryListElement2.setName("Cięgna drzwi");
+        autopart.addCategoryToList(categoryListElement2);
+
+        //when
+        AllegroCategoryFinder allegroCategoryFinder = new AllegroCategoryFinder();
+        String s = allegroCategoryFinder.MatchCategories(autopart);
+
+        //then
+        assertThat(s, is(equalTo(" -> Części karoserii -> Cięgna drzwi")));
+    }
 }
