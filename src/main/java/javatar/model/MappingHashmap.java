3cb9b4100a73686ea2135e562416d907c4e6b4b4
daniel-kepczynski <danielkepczynski@gmail.com>
2016-04-10 15:46:33 +0200

    HashMap

---
+++ src/main/java/javatar/model/MappingHashmap.java
@@ -0,0 +1,27 @@
+package javatar.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MappingHashmap {
+
+    public HashMap<String, String> JsonXmlMapping = new HashMap<String, String>() {{
+        put("","");
+        put("","");
+        put("","");
+        put("","");
+        put("","");
+        put("","");
+        put("","");
+        put("","");
+        put("","");
+        put("","");
+        put("","");
+        put("","");
+        put("","");
+        put("","");
+        put("","");
+    }};
+
+
+}
