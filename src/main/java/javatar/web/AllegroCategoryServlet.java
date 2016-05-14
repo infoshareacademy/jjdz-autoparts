8030d5fe27f00180f4070e2fe82501b244adc700
Aneta Karbowska <karbowska.aneta@gmail.com>
2016-05-14 13:25:24 +0200

    Allegro Category Servlet z nazwą części w linku

--- src/main/java/javatar/web/AllegroCategoryServlet.java
+++ src/main/java/javatar/web/AllegroCategoryServlet.java
@@ -26,7 +26,7 @@ public class AllegroCategoryServlet extends HttpServlet {
     XMLParser xmlParser = new XMLParser();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         AutopartAllegroListModel autopartAllegroListModel = new AutopartAllegroListModel();
         Autopart autopart = new Autopart();
@@ -81,12 +81,12 @@ public class AllegroCategoryServlet extends HttpServlet {
         autopartAllegroListModel.setAllegroCategories(allegroCategoriesList);
         System.out.println(autopartAllegroListModel.getAllegroCategories().size());
         System.out.println(autopartAllegroListModel.getAutopart().toString());
-        String allegroLink = createAllegroLink.createAllegroLink(autopartAllegroListModel);
+        String allegroLink = createAllegroLink.createAllegroLink(autopartAllegroListModel) + "?string=" + autopart.getName() + " " + autopart.getBrand() + " " + autopart.getId();
 
         req.setAttribute("allegroLink", allegroLink);
-        req.setAttribute("partName",name);
-        req.setAttribute("partBrand",brand);
-        req.setAttribute("partId",id);
+        req.setAttribute("partName", name);
+        req.setAttribute("partBrand", brand);
+        req.setAttribute("partId", id);
 
         RequestDispatcher dispatcher = req.getRequestDispatcher("AllegroCategoryForm.jsp");
         dispatcher.forward(req, resp);
