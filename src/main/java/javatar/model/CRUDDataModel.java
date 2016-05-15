15e0877937148c353fbbf62932970c2403a62bab
AnetaK <karbowska.aneta@gmail.com>
2016-05-15 16:09:26 +0200

    Frontend aneta (#11)
    * Loggery
    
    * Autentyczny frontend początki
    
    * Autentyczny frontend II
    
    * tmp - crud początki
    
    * merge z Developem i usunięcie id

---
+++ src/main/java/javatar/model/CRUDDataModel.java
@@ -0,0 +1,36 @@
+package javatar.model;
+
+//@Entity
+public class CRUDDataModel {
+//    @Id
+//    Long id;
+//
+//    Long UserId;
+//
+//    @Embedded
+//    FormData formData;
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public Long getUserId() {
+//        return UserId;
+//    }
+//
+//    public void setUserId(Long userId) {
+//        UserId = userId;
+//    }
+//
+//    public FormData getFormData() {
+//        return formData;
+//    }
+//
+//    public void setFormData(FormData formData) {
+//        this.formData = formData;
+//    }
+}
