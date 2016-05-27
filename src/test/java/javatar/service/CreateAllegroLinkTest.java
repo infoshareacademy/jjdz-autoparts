74df35084249889d06e7337cb8cd2d7e60ea4282
AnetaK <karbowska.aneta@gmail.com>
2016-05-27 17:21:36 +0200

    Poprawione testy jednostkowe

--- src/test/java/javatar/service/CreateAllegroLinkTest.java
+++ src/test/java/javatar/service/CreateAllegroLinkTest.java
@@ -4,6 +4,7 @@ import javatar.model.AllegroCategories;
 import javatar.model.Autopart;
 import javatar.model.AutopartAllegroListModel;
 import javatar.model.AutopartCategory;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +13,7 @@ import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -19,118 +21,113 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class CreateAllegroLinkTest {
-//    CreateAllegroLink createAllegroLink = new CreateAllegroLink();
-//    AutopartAllegroListModel autopartAllegroListModel = null;
-//    XMLParser xmlParser = null;
-//
-//    @BeforeClass
-//    public static void initialize() throws IOException {
-//        XMLParser xmlParser = null;
-//        AutopartAllegroListModel autopartAllegroListModel = null;
-//        InputStream fileReader = new FileInputStream("src/main/resources/Allegro_cathegories_2016-02-13.xml");
-//        List<AllegroCategories> allegroCategoriesList = xmlParser.allegroCategoryObject(fileReader);
-//        autopartAllegroListModel.setAllegroCategories(allegroCategoriesList);
-//
-//    }
-//
-//
-//    @Test
-//    public void test_create_allegro_link() throws Exception {
-//        InputStream fileReader = new FileInputStream("resources/Allegro_cathegories_2016-02-13.xml");
-//        List<AllegroCategories> allegroCategoriesList = xmlParser.allegroCategoryObject(fileReader);
-//        autopartAllegroListModel.setAllegroCategories(allegroCategoriesList);
-//
-//        Autopart autopart = new Autopart();
-//        AutopartCategory categoryListElement = new AutopartCategory();
-//        categoryListElement.setName("Układ chłodzenia");
-//        autopart.addCategoryToList(categoryListElement);
-//        autopartAllegroListModel.setAutopart(autopart);
-//
-//        String link = createAllegroLink.createAllegroLink(autopartAllegroListModel);
-//
-//        assertThat(link,is(equalTo("http://allegro.pl/czesci-samochodowe-chlodzenie-silnika-18689")));
-//
-//    }
-//
-//    @Test
-//    public void test_create_allegro_link_no_subcategory() throws Exception {
-//        Autopart autopart = new Autopart();
-//        AutopartCategory categoryListElement = new AutopartCategory();
-//        categoryListElement.setName("Dwużlad");
-//        autopart.addCategoryToList(categoryListElement);
-//        autopartAllegroListModel.setAutopart(autopart);
-//
-//        String link = createAllegroLink.createAllegroLink(autopartAllegroListModel);
-//
-//        assertThat(link,is(equalTo("http://allegro.pl/czesci-samochodowe-620")));
-//
-//    }
-//
-//    @Test
-//    public void test_create_allegro_link_chlodnice() throws Exception {
-//        Autopart autopart = new Autopart();
-//        AutopartCategory categoryListElement = new AutopartCategory();
-//        categoryListElement.setName("Chłodzenie silnika");
-//        autopart.addCategoryToList(categoryListElement);
-//        AutopartCategory categoryListElement2 = new AutopartCategory();
-//        categoryListElement2.setName("Chłodnice");
-//        autopart.addCategoryToList(categoryListElement2);
-//        AutopartCategory categoryListElement3 = new AutopartCategory();
-//        categoryListElement3.setName("Chłodnice oleju");
-//        autopart.addCategoryToList(categoryListElement3);
-//        autopartAllegroListModel.setAutopart(autopart);
-//
-//        String link = createAllegroLink.createAllegroLink(autopartAllegroListModel);
-//
-//        assertThat(link,is(equalTo("http://allegro.pl/chlodnice-chlodnice-oleju-251083")));
-//
-//    }
-//
-//    @Test
-//    public void test_match_2_categories() throws Exception {
-//
-//        //given
-//        Autopart autopart = new Autopart();
-//        AutopartCategory categoryListElement = new AutopartCategory();
-//        categoryListElement.setName("Części karoserii");
-//        autopart.addCategoryToList(categoryListElement);
-//        AutopartCategory categoryListElement2 = new AutopartCategory();
-//        categoryListElement2.setName("Maski");
-//        autopart.addCategoryToList(categoryListElement2);
-//        autopartAllegroListModel.setAutopart(autopart);
-//
-//        String link = createAllegroLink.createAllegroLink(autopartAllegroListModel);
-//
-//        assertThat(link,is(equalTo("http://allegro.pl/czesci-samochodowe-czesci-karoserii-4094")));
-//    }
-//
-//    @Test
-//    public void test_does_not_match_any_category() throws Exception {
-//
-//        //given
-//        Autopart autopart = new Autopart();
-//
-//        String link = createAllegroLink.createAllegroLink(autopartAllegroListModel);
-//
-//        assertThat(link,is(equalTo("http://allegro.pl/czesci-samochodowe-620")));
-//    }
-//
-//    @Test
-//    public void test_matching_category_from_HashMap() {
-//        Autopart autopart = new Autopart();
-//        AutopartCategory categoryListElement = new AutopartCategory();
-//        categoryListElement.setName("Układ chłodzenia");
-//        autopart.addCategoryToList(categoryListElement);
-//        AutopartCategory categoryListElement2 = new AutopartCategory();
-//        categoryListElement2.setName("Chłodnice");
-//        autopart.addCategoryToList(categoryListElement2);
-//        autopartAllegroListModel.setAutopart(autopart);
-//
-//        //when
-//        String link = createAllegroLink.createAllegroLink(autopartAllegroListModel);
-//
-//        assertThat(link,is(equalTo("http://allegro.pl/chlodzenie-silnika-chlodnice-18690")));
-//
-//    }
+    AllegroCategoryFinder allegroCategoryFinder = new AllegroCategoryFinder();
+    CreateAllegroLink createAllegroLink = new CreateAllegroLink();
+    AutopartAllegroListModel autopartAllegroListModel = new AutopartAllegroListModel();
+    XMLParser xmlParser = new XMLParser();
+    List<AllegroCategories> allegroCategoriesList = new ArrayList<>();
+
+    @Before
+    public void initialize() {
+        InputStream allegroCategoriesFile = this.getClass().getResourceAsStream("/Allegro_cathegories_2016-02-13.xml");
+        allegroCategoriesList = xmlParser.allegroCategoryObject(allegroCategoriesFile);
+        autopartAllegroListModel.setAllegroCategories(allegroCategoriesList);
+    }
+
+    @Test
+    public void test_create_allegro_link() throws Exception {
+        Autopart autopart = new Autopart();
+        AutopartCategory categoryListElement = new AutopartCategory();
+        categoryListElement.setName("Układ chłodzenia");
+        autopart.addCategoryToList(categoryListElement);
+        autopartAllegroListModel.setAutopart(autopart);
+
+        String link = createAllegroLink.createAllegroLink(autopartAllegroListModel);
+
+        assertThat(link,is(equalTo("http://allegro.pl/czesci-samochodowe-chlodzenie-silnika-18689")));
+
+    }
+
+    @Test
+    public void test_create_allegro_link_no_subcategory() throws Exception {
+        Autopart autopart = new Autopart();
+        AutopartCategory categoryListElement = new AutopartCategory();
+        categoryListElement.setName("Dwużlad");
+        autopart.addCategoryToList(categoryListElement);
+        autopartAllegroListModel.setAutopart(autopart);
+
+        String link = createAllegroLink.createAllegroLink(autopartAllegroListModel);
+
+        assertThat(link,is(equalTo("http://allegro.pl/czesci-samochodowe-620")));
+
+    }
+
+    @Test
+    public void test_create_allegro_link_chlodnice() throws Exception {
+        Autopart autopart = new Autopart();
+        AutopartCategory categoryListElement = new AutopartCategory();
+        categoryListElement.setName("Chłodzenie silnika");
+        autopart.addCategoryToList(categoryListElement);
+        AutopartCategory categoryListElement2 = new AutopartCategory();
+        categoryListElement2.setName("Chłodnice");
+        autopart.addCategoryToList(categoryListElement2);
+        AutopartCategory categoryListElement3 = new AutopartCategory();
+        categoryListElement3.setName("Chłodnice oleju");
+        autopart.addCategoryToList(categoryListElement3);
+        autopartAllegroListModel.setAutopart(autopart);
+
+        String link = createAllegroLink.createAllegroLink(autopartAllegroListModel);
+
+        assertThat(link,is(equalTo("http://allegro.pl/chlodnice-chlodnice-oleju-251083")));
+
+    }
+
+    @Test
+    public void test_match_2_categories() throws Exception {
+
+        //given
+        Autopart autopart = new Autopart();
+        AutopartCategory categoryListElement = new AutopartCategory();
+        categoryListElement.setName("Części karoserii");
+        autopart.addCategoryToList(categoryListElement);
+        AutopartCategory categoryListElement2 = new AutopartCategory();
+        categoryListElement2.setName("Maski");
+        autopart.addCategoryToList(categoryListElement2);
+        autopartAllegroListModel.setAutopart(autopart);
+
+        String link = createAllegroLink.createAllegroLink(autopartAllegroListModel);
+
+        assertThat(link,is(equalTo("http://allegro.pl/czesci-samochodowe-czesci-karoserii-4094")));
+    }
+
+    @Test
+    public void test_does_not_match_any_category() throws Exception {
+
+        //given
+        Autopart autopart = new Autopart();
+        autopartAllegroListModel.setAutopart(autopart);
+
+        String link = createAllegroLink.createAllegroLink(autopartAllegroListModel);
+
+        assertThat(link,is(equalTo("http://allegro.pl/czesci-samochodowe-620")));
+    }
+
+    @Test
+    public void test_matching_category_from_HashMap() {
+        Autopart autopart = new Autopart();
+        AutopartCategory categoryListElement = new AutopartCategory();
+        categoryListElement.setName("Układ chłodzenia");
+        autopart.addCategoryToList(categoryListElement);
+        AutopartCategory categoryListElement2 = new AutopartCategory();
+        categoryListElement2.setName("Chłodnice");
+        autopart.addCategoryToList(categoryListElement2);
+        autopartAllegroListModel.setAutopart(autopart);
+
+        //when
+        String link = createAllegroLink.createAllegroLink(autopartAllegroListModel);
+
+        assertThat(link,is(equalTo("http://allegro.pl/chlodzenie-silnika-chlodnice-18690")));
+
+    }
 
 }
