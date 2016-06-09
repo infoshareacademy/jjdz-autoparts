fffc9edc4538b21fb732884a134a2d149210ba2a
MichalC <michal.cymerys@gmail.com>
2016-06-09 20:17:15 +0200

    usunięte stare TODO

--- src/main/java/javatar/model/JsonData.java
+++ src/main/java/javatar/model/JsonData.java
@@ -2,11 +2,8 @@ package javatar.model;
 
 import java.util.Collection;
 
-// TODO: 10.03.16 czy nie lepiej uniwersalna klasa zamiast klasa do kazdej podstrony jsona?
 public class JsonData {
     private String datatype;
-    // TODO: 10.03.16 czy da radę uniwersalna kolekcję? potem określany będzie typ danych?
-//    private Collection<Object> data;
     private Collection<Autopart> data;
 
     public String getDatatype() {
