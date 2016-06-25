90a989efa8a8d6f885365e557c8c216279d19d34
Aneta Karbowska <karbowska.aneta@gmail.com>
2016-06-25 13:30:19 +0200

    Raport przyjmujący dane

--- report-module/src/main/java/reports/searched/part/GetSearchedPart.java
+++ report-module/src/main/java/reports/searched/part/GetSearchedPart.java
@@ -1,4 +1,31 @@
 package reports.searched.part;
 
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Stateless
+@Path("/searched")
 public class GetSearchedPart   {
+
+    @Inject
+    StorePart store;
+
+    @POST
+    @Path("/part")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response readPartFromJson (@NotNull PartModel part){
+
+        Long id = store.save(part);
+
+        return Response.noContent().build();
+    }
+
+
+
 }
---
+++ report-module/src/main/java/reports/searched/part/StorePart.java
@@ -0,0 +1,21 @@
+package reports.searched.part;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class StorePart {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Long save (PartModel part){
+        em.persist(part);
+        return part.getId();
+    }
+
+    public PartModel getPartById (Long id){
+        return em.find(PartModel.class, id);
+    }
+}
