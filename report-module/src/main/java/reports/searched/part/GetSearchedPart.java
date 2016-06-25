765b2b79fe5f4f358ebe69411203716c79820b82
Aneta Karbowska <karbowska.aneta@gmail.com>
2016-06-25 12:56:51 +0200

    Struktura

---
+++ report-module/src/main/java/reports/searched/part/GetSearchedPart.java
@@ -0,0 +1,8 @@
+package reports.searched.part;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class GetSearchedPart extends Application {
+}
