ef87ee94d2bb3b12760b17076ee0a75ec2034b06
daniel-kepczynski <danielkepczynski@gmail.com>
2016-06-26 15:29:26 +0200

    Test IT do resta z userami

--- App/src/test/java/javatar/service/UserResourceIT.java
+++ App/src/test/java/javatar/service/UserResourceIT.java
@@ -3,64 +3,77 @@ package javatar.service;
 import com.google.gson.Gson;
 import javatar.RestApplication;
 import javatar.model.AccountType;
+import javatar.model.AdminEmail;
 import javatar.model.GlobalUser;
 import javatar.model.LinkedInUser;
 import javatar.web.GlobalUserService;
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.security.auth.spi.Users;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.ejb.EJB;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
 import java.io.File;
 import java.net.URI;
 import java.net.URL;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+
+//@Ignore
 @RunWith(Arquillian.class)
 public class UserResourceIT {
 
     @Deployment
-    public static WebArchive create() {
+    public static WebArchive deployment() {
 
-        File libs[] = Maven.resolver()
-                .resolve("org.apache.logging.log4j:log4j-core:2.5").withTransitivity()
+        File[] libs = Maven.resolver()
+                .resolve("org.apache.logging.log4j:log4j-core:2.5",
+                        "org.apache.logging.log4j:log4j-api:2.5",
+                        "com.google.code.gson:gson:2.6.1")
+                .withTransitivity()
                 .asFile();
 
         return ShrinkWrap.create(WebArchive.class)
                 .addAsLibraries(libs)
                 .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
-                .addAsResource("log4j2.xml", "log4j2.xml")
+                // .addAsResource("log4j2.xml", "log4j2.xml")
                 .addClass(RestApplication.class)
                 .addClass(GlobalUser.class)
                 .addClass(GlobalUserService.class)
                 .addClass(LinkedInUser.class)
                 .addClass(AccountType.class)
-                .addClass(UserResource.class);
+                .addClass(UserResource.class)
+                .addClass(AdminEmail.class);
+        // .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
-    static LinkedInUser linkedInUser;
 
     @EJB
     GlobalUserService globalUserService;
 
-    static URI create_user_location;
+    //static URI create_user_location = ;
 
-    @Before
-    public void setup_linkedin_user_and_database() {
+    @PersistenceContext
+    EntityManager em;
+
+
+    @Test
+    @InSequence(1)
+    public void add_user_to_database() {
 
         String linkedinJson = "{\n" +
                 "\"emailAddress\": \"danielkepczynski@gmail.com\",\n" +
@@ -68,44 +81,35 @@ public class UserResourceIT {
                 " \"lastName\": \"Kępczyński\"\n" +
                 "}";
         Gson gson = new Gson();
-        linkedInUser = gson.fromJson(linkedinJson, LinkedInUser.class);
+        LinkedInUser linkedInUser = gson.fromJson(linkedinJson, LinkedInUser.class);
 
+        globalUserService.getGLobalUser(linkedInUser);
+
+
+        List<GlobalUser> list = em.createQuery("from GlobalUser", GlobalUser.class).getResultList();
+
+        assertThat(list.size(), is(1));
     }
 
 
-
     @Test
-    @RunAsClient
+    @InSequence(2)
     public void should_return_a_global_user(@ArquillianResource URL baseUrl) throws Exception {
-
-        GlobalUser user = globalUserService.getGLobalUser(linkedInUser);
 
         URI uri = UriBuilder.fromUri(baseUrl.toString())
                 .segment("api")
                 .segment("users")
-                .segment("all")
+                .segment("1")
                 .build();
 
         GlobalUser globalUser = ClientBuilder.newClient()
-                .target(create_user_location)
+                .target(uri.toString())
                 .request()
                 .accept(MediaType.APPLICATION_JSON_TYPE + ";charset=utf-8")
                 .get(GlobalUser.class);
 
         assertThat(globalUser.geteMail(), is("danielkepczynski@gmail.com"));
 
-
     }
 
-//    @Ignore
-//    @Test
-//    public void should_return_all_user() throws Exception {
-//
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void Should_return_users_with_rights_to_reports() throws Exception {
-//
-//    }
 }
