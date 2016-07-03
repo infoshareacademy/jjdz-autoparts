68e34345e26ba0ab876ae256c79245d0a38af6f8
AnetaK <karbowska.aneta@gmail.com>
2016-07-03 21:50:32 +0200

    Porządki

--- App/src/main/java/javatar/web/AllegroCategoriesCache.java
+++ App/src/main/java/javatar/web/AllegroCategoriesCache.java
@@ -10,7 +10,6 @@ import javax.ejb.Lock;
 import javax.ejb.LockType;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,16 +19,13 @@ import java.util.List;
 public class AllegroCategoriesCache {
 
     private static final Logger LOGGER = LogManager.getLogger();
-    //    XMLParser xmlParser = new XMLParser();
     List<AllegroCategories> allegroCategoriesList = new ArrayList<>();
     APIallegro apIallegro = new APIallegro();
 
     @PostConstruct
     public void initialize() {
 
-        InputStream allegroCategoriesFile = this.getClass().getResourceAsStream("/Allegro_cathegories_2016-02-13.xml");
         allegroCategoriesList = apIallegro.getAllegroCategoriesList();
-//        allegroCategoriesList = xmlParser.allegroCategoryObject(allegroCategoriesFile);
         LOGGER.info("allegroCategoriesList has size: {}", allegroCategoriesList.size());
 
     }
