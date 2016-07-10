58f01cf2dce63bf42a5b4ef5dbe6453016f4e0b4
michal.cymerys <michal.cymerys@gmail.com>
2016-07-10 17:20:17 +0200

    servis wysyłający maila, parametry pobierane z pliku properties

--- report-module/src/main/java/reports/mailing/EmailService.java
+++ report-module/src/main/java/reports/mailing/EmailService.java
@@ -1,6 +1,4 @@
-package javatar.service;
-
-import javatar.model.Email;
+package reports.mailing;
 
 import javax.ejb.Stateless;
 import javax.mail.Message;
@@ -9,25 +7,56 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.Properties;
 
 @Stateless
 public class EmailService {
 
-    private String SMTP_AUTH_PWD = "autoparts";
-    private String SMTP_HOST_NAME = "smtp.gmail.com";
-    private int SMTP_HOST_PORT = 465;
-    private String SMTP_AUTH_USER = "autoparts.reporting@gmail.com";
+    InputStream inputStream;
+    private String SMTP_AUTH_PWD;
+    private String SMTP_HOST_NAME;
+    private int SMTP_HOST_PORT;
+    private String SMTP_AUTH_USER;
 
-    public void sendMail(Email email) throws MessagingException {
-        //LOG.info("Sender: {} Receiver: {} Tittle: {} Body: \n {}", email.getSender(), email.getReceiver(), email.getTitle(), email.getBody());
-        //getParametersFromDatabase();
+    public void sendMail(Email email) throws MessagingException, IOException {
+
         Properties props = createProperties(SMTP_AUTH_USER);
+        getParametersFromFile();
         Session mailSession = createMailSession(props);
         MimeMessage message = createMessage(mailSession, email);
         sendMessage(mailSession, message);
-        //LOG.info("Message sent using zoho account");
+    }
+
+    private void getParametersFromFile() throws IOException{
+        try {
+            Properties prop = new Properties();
+            String propFileName = "email.properties";
+
+            inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+            }
+
+            Date time = new Date(System.currentTimeMillis());
+
+            SMTP_AUTH_PWD = prop.getProperty("SMTP_AUTH_PWD");
+            SMTP_HOST_NAME = prop.getProperty("SMTP_HOST_NAME");
+            SMTP_HOST_PORT = Integer.parseInt(prop.getProperty("SMTP_HOST_PORT"));
+            SMTP_AUTH_USER = prop.getProperty("SMTP_AUTH_USER");
+
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        } finally {
+            inputStream.close();
+        }
     }
 
     private Properties createProperties(String sender) {
@@ -53,7 +82,7 @@ public class EmailService {
     private MimeMessage createMessage(Session mailSession, Email email) throws MessagingException {
         MimeMessage message = new MimeMessage(mailSession);
         message.setSubject(email.getTitle());
-        message.setContent(email.getBody(), "text/plain; charset=UTF-8");
+        message.setContent(email.getMessage(), "text/plain; charset=UTF-8");
         message.addRecipient(Message.RecipientType.TO, new InternetAddress(email.getReceiver()));
 
         try {
