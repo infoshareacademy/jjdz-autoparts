ddb7a3a6cf8ee59e1d4be9743fe3b173c1db3f21
AnetaK <karbowska.aneta@gmail.com>
2016-07-14 13:14:33 +0200

    poczatki

--- report-module/src/main/java/reports/searched/part/MostFrequentlySearchedPartsReport.java
+++ report-module/src/main/java/reports/searched/part/MostFrequentlySearchedPartsReport.java
@@ -4,6 +4,7 @@ import org.hibernate.sql.Select;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class MostFrequentlySearchedPartsReport {
@@ -12,8 +13,8 @@ public class MostFrequentlySearchedPartsReport {
     EntityManager em;
 
 
-    private List<PartForReportModule> getAllPartsForReport() {
-        return em.createQuery("select p from PartForReportModule p", PartForReportModule.class)
+    private List<PartForReportModule> getAllPartsForReport(LocalDateTime startDate, LocalDateTime endDate, PartSearchSource source) {
+        return em.createQuery("select p. from PartForReportModule p", PartForReportModule.class)
                 .getResultList();
     }
 
