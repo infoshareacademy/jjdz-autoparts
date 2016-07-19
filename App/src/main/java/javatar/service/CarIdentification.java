246a21ca56c3edcb2b17fe10e4882d3c0050d246
cymekcym <michal.cymerys@gmail.com>
2016-07-19 19:09:11 +0200

    Aztec code on website (#43)
    * początki aztec code na stronie
    
    * zostalo tylko parsowanie danych z api na samochod z jsona
    
    * jeszcze kilka poprawek
    
    * poprawka
    
    * .
    
    * .
    
    * dziala api
    
    * dokoczyc wyszukiwanie
    
    * final version
    
    * początki aztec code na stronie
    
    * zostalo tylko parsowanie danych z api na samochod z jsona
    
    * jeszcze kilka poprawek
    
    * poprawka
    
    * .
    
    * .
    
    * dziala api
    
    * dokoczyc wyszukiwanie
    
    * final version
    
    * przygotowanie do pull request
    
    * system out -> logger

--- App/src/main/java/javatar/service/CarIdentification.java
+++ App/src/main/java/javatar/service/CarIdentification.java
@@ -1,178 +1,98 @@
 package javatar.service;
 
-import javatar.model.Car;
-import javatar.model.CarsBrands;
-import javatar.model.CarsEngineAndFuel;
-import javatar.model.CarsModels;
+import javatar.model.*;
+import javatar.web.BrandsJsonCache;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.HashMap;
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
-import java.util.Scanner;
 
-
+@Stateless
 public class CarIdentification {
 
-    final String mainPath = "src/main/resources/json_data/";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    @EJB
+    BrandsJsonCache cache;
 
     public CarIdentification() {
     }
 
-    public Car FindingCarByAztecCodeAnswer(Car carIn) throws FileNotFoundException {
+    public Car FindingCarByAztecCodeAnswer(CarFromAztecData apiAnswer) throws FileNotFoundException {
 
+        LOGGER.debug("Looking for car: brand - " + apiAnswer.getCarBrand() + " ; model - " + apiAnswer.getCarModel());
+        Car foundCar = new Car();
 
-        CarsBrands carInBrand = carIn.getCarsBrand();
-        CarsModels canInModel = carIn.getCarsModel();
-        String carInEngineCapacity = carIn.getEngineCapacity();
-        String carInFuelType = carIn.getFuelType();
-        String carInEnginePower = carIn.getEnginePower();
-        int carInProductionYear = carIn.getProductionYear();
-        //CarsEngineAndFuel carInEngine =carIn.getEngine();
-        CarsEngineAndFuel engineOut = new CarsEngineAndFuel();
+        CarsBrands brand = findBrand(apiAnswer.getCarBrand());
 
-        JsonParserBrands brands = new JsonParserBrands();
-        String carOutId = brands.searchCarId(carInBrand.getName());
-        JsonParserModels models = new JsonParserModels(mainPath + "/" + carOutId + ".json");
-        String modelOutId = models.searchCarId(canInModel.getName(), carInProductionYear);
-        JsonParserEngine engine = new JsonParserEngine(mainPath + "/" + modelOutId + ".json");
-        List<CarsEngineAndFuel> carEnginesList = engine.searchEngineTypeByTokens(carInFuelType, carInEngineCapacity, carInEnginePower);
+        CarsModels model = findModel(brand.getLink(), apiAnswer.getCarModel(), apiAnswer.getProductionYear());
 
+        foundCar.setCarsBrand(brand);
+        foundCar.setCarsModel(model);
 
-        if (carEnginesList.size() > 1) {
-            System.out.println("Znaleziono " + carEnginesList.size() + " typy silników pasujące do danych zawartych w kodzie Aztek");
-            System.out.println("Wybierz właściwy silnik i wprowadź jego numer");
-            for (int i = 0; i < carEnginesList.size(); i++) {
-                System.out.println(carEnginesList.get(i));
-                i++;
-            }
-            Scanner scanner = new Scanner(System.in);
-            int numberFromEnginesList = scanner.nextInt();
-            engineOut = carEnginesList.get(numberFromEnginesList);
-
-        } else {
-            engineOut = carEnginesList.get(0);
-        }
-
-        carInBrand.setId(carOutId);
-        canInModel.setId(modelOutId);
-//        carInEngine.setId(engineOut.getId());
-        carIn.setCarsBrand(carInBrand);
-        carIn.setCarsModel(canInModel);
-//        carIn.setEngine(carInEngine);
-        carIn.setEngineID(engineOut.getId());
-        System.out.println(carIn);
-
-        return carIn;
+        LOGGER.debug("Found carBrand: " + foundCar.getCarsBrand().toString());
+        LOGGER.debug("Found carModel: " + foundCar.getCarsModel().toString());
+        return foundCar;
     }
 
-    public Car FindingCarManagement() throws IOException {
-        String brand = new String();
-        Scanner scanner = new Scanner(System.in);
-        JsonParserBrands brandFileName = new JsonParserBrands();
-        String brandFileNameOut = "Error";
+    private CarsModels findModel(String link, String carModel, String productionYearString) {
+        LOGGER.debug("Looking for car model: " + carModel);
+        JsonParserAll parser = new JsonParserAll();
 
-        //------------------------------------------------------------------------------------------------------
-        //Car brands section
-        //------------------------------------------------------------------------------------------------------
-        //while (brandFileNameOut == "Error") {
-        System.out.println("Wprowadź nazwę marki lub wybierz z listy i wprowadź przyporządkowany jej numer:\r\n1. OPEL\r\n2. VOLKSWAGEN");
-        String brandSelection = scanner.nextLine();
+        String url = "http://infoshareacademycom.2find.ru" + link + "?lang=polish";
+        DataCarsModels dataCarsModels = parser.parseModelFile(url);
+        Collection<CarsModels> carsModelsCollection = dataCarsModels.getData();
 
-        if (brandSelection.length() == 1) {
-            switch (brandSelection) {
-                case "1":
-                    brand = "OPEL";
-                    break;
-                case "2":
-                    brand = "VOLKSWAGEN";
-                    break;
-                default:
-                    brand = "Error";
-                    break;
+        Integer startYear;
+        Integer endYear;
+        Integer productionYear = Integer.parseInt(productionYearString);
+
+        for (CarsModels model :
+                carsModelsCollection) {
+
+            startYear = model.getStart_year();
+            endYear = (model.getEnd_year() == null) ? LocalDateTime.now().getYear() : model.getEnd_year();
+
+            if (model.getName().toUpperCase().contains(carModel.toUpperCase())
+                    && startYear <= productionYear
+                    && endYear >= productionYear){
+
+                LOGGER.info("Found: " + model.toString());
+                return model;
             }
-        } else {
-            brand = brandSelection;
         }
+        LOGGER.error("MODEL NOT FOUND!");
+        return null;
+    }
 
+    private CarsBrands findBrand(String carBrand) {
+        LOGGER.debug("Looking for car brand: " + carBrand);
 
-        brandFileNameOut = brandFileName.searchCarId(brand);
-        // }
+        Collection<CarsBrands> carsBrandsCollection = cache.returnBrandsCollection();
 
-        //------------------------------------------------------------------------------------------------------
-        //Car models section
-        //------------------------------------------------------------------------------------------------------
-        JsonParserModels modelId = new JsonParserModels(mainPath + brandFileNameOut + ".json");
-        String modelPath = mainPath + brandFileNameOut;
-        String modelFileNameOut = "Error";
-        String model = new String();
-        String year = new String();
-
-        //while (modelFileNameOut == "Error") {
-        System.out.println("Wprowadź nazwę modelu lub wybierz z listy i wprowadź przyporządkowany jej numer:\r\n1. ASTRA G kombi\r\n2. JETTA VII SportWagon");
-        String modelSelection = scanner.nextLine();
-        System.out.println("Wprowadź rok produkcji:");
-        year = scanner.nextLine();
-
-
-        if (modelSelection.length() == 1) {
-            switch (modelSelection) {
-                case "1":
-                    model = "ASTRA G kombi";
-                    break;
-                case "2":
-                    model = "JETTA VII SportWagon";
-                    break;
-                default:
-                    model = "Error";
-                    break;
+        for (CarsBrands brand :
+                carsBrandsCollection) {
+            if (brand.getName().toUpperCase().contains(carBrand.toUpperCase())){
+                LOGGER.info("Brand found: " + brand.toString());
+                return brand;
             }
-        } else {
-            model = modelSelection;
         }
+        LOGGER.error("BRAND NOT FOUND!");
+        return null;
+    }
 
-        modelFileNameOut = modelId.searchCarId(model, Integer.parseInt(year));
-        // }
-        //------------------------------------------------------------------------------------------------------
-        //CEngines and fuel section
-        //------------------------------------------------------------------------------------------------------
-        String engineFileNameOut = "Error";
-        final JsonParserEngine engine = new JsonParserEngine(mainPath + modelFileNameOut + ".json");
+    public List<CarsEngineAndFuel> findMatchingEngines(String url, CarFromAztecData aztecData) {
 
-        while (engineFileNameOut == "Error") {
+        JsonParserAll parser = new JsonParserAll();
+        DataCarsEngineAndFuel engines = parser.parseEngineFile(url);
 
-            System.out.println("Wpisz numer przyporządkowany właściwemu silnikowi:");
-
-            HashMap<Integer, String> engineIdsMap = engine.listAllEngineTypes();
-            String engineSelection = scanner.nextLine();
-//
-//            while (Integer.parseInt(engineSelection) > (engineIdsMap.size() - 1) || Integer.parseInt(engineSelection) < 0) {
-//                System.out.println("Wpisz numer przyporządkowany właściwemu silnikowi:");
-//                engineSelection = scanner.nextLine();
-//            }
-
-            engineFileNameOut = engineIdsMap.get(Integer.parseInt(engineSelection));
-        }
-
-        JsonParserEngine engine1 = new JsonParserEngine(mainPath + modelFileNameOut + ".json");
-        //TODO dopytaj dlaczego nie możesz tu skorzystać z engine
-        String engineName = engine1.searchEngineTypeByNumber(engineFileNameOut);
-
-
-        Car car = new Car();
-        car.setCarsBrand(new CarsBrands(brandFileNameOut, brand));
-        car.setCarsModel(new CarsModels(modelFileNameOut, model));
-//        car.setBrandName(brand);
-//        car.setBrandId(brandFileNameOut);
-//        car.setModelId(modelFileNameOut);
-//        car.setModelName(model);
-        car.setProductionYear(Integer.parseInt(year));
-        car.setTypeId(engineFileNameOut);
-        car.setTypeName(engineName);
-
-        System.out.println(car);
-
-        return car;
+        return parser.searchEngineTypeByTokens(engines, aztecData.getFuelType(), aztecData.getEngineCapacity(), aztecData.getEnginePower());
     }
 }
 
---
+++ App/src/main/java/javatar/web/AztecKeySearchServlet.java
@@ -0,0 +1,220 @@
+package javatar.web;
+
+import javatar.model.*;
+import javatar.service.CarIdentification;
+import javatar.service.GetJsonFromAtenaApi;
+import javatar.service.JsonParserAll;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@WebServlet(urlPatterns = "/AztecKey")
+public class AztecKeySearchServlet extends HttpServlet {
+
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	@EJB
+	BrandsJsonCache cache;
+
+	@EJB
+	CarIdentification carIdentification;
+
+	@Inject
+	FormData formData;
+
+	@EJB
+	GetJsonFromAtenaApi getJsonFromAtenaApi;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		JsonParserAll parser = new JsonParserAll();
+		Collection<CarsBrands> carsBrandsCollection = cache.returnBrandsCollection();
+
+		String url;
+
+		req.setCharacterEncoding("UTF-8");
+
+		LOGGER.info("cache return collection size: {}", carsBrandsCollection.size());
+
+		String userSessionApiKey = req.getParameter("apiKey");
+
+		LOGGER.info("user key: {}", userSessionApiKey);
+
+		String carDataFromAtenaApi = getJsonFromAtenaApi.getCarFromAtenaApi(userSessionApiKey);
+
+		LOGGER.info("carDataFromAtenaApi: {} ", carDataFromAtenaApi);
+
+		CarFromAztecData aztecData = getCarFromApiString(carDataFromAtenaApi);
+
+		Car carFromApi = new Car();
+		carFromApi.setCarsBrand(null);
+
+		if (aztecData != null) {
+			carFromApi = carIdentification.FindingCarByAztecCodeAnswer(aztecData);
+		}
+
+
+		if (carFromApi.getCarsBrand() == null) {
+			// TODO: 11.07.16 info dla użytkownika, że nie znaleziono branda
+			req.setCharacterEncoding("UTF-8");
+
+			req.setAttribute("brands", carsBrandsCollection);
+			LOGGER.info("carsBransdCollection has size: {}", carsBrandsCollection.size());
+			RequestDispatcher dispatcher = req.getRequestDispatcher("CarBranchChoosingForm.jsp");
+			dispatcher.forward(req, resp);
+		} else if (carFromApi.getCarsModel() == null) {
+			req.setCharacterEncoding("UTF-8");
+
+			String brandName = carFromApi.getCarsBrand().getName();
+			String brandLink = carFromApi.getCarsBrand().getLink();
+
+			req.setAttribute("brandName", brandName);
+
+			formData.setCarBrand(brandName);
+
+			url = "http://infoshareacademycom.2find.ru" + brandLink + "?lang=polish";
+
+			LOGGER.info("Chosen model file name: {}; resources link: {}", brandName, url);
+			DataCarsModels dataCarsModels = parser.parseModelFile(url);
+			LOGGER.info("Data parsed on model file has size: {}", dataCarsModels.getData().size());
+			req.setAttribute("models", dataCarsModels.getData());
+
+			RequestDispatcher dispatcher = req.getRequestDispatcher("CarModelChoosingForm.jsp");
+			dispatcher.forward(req, resp);
+		} else {
+
+			String modelLink = carFromApi.getCarsModel().getLink();
+			url = "http://infoshareacademycom.2find.ru" + modelLink + "?lang=polish";
+
+			List<CarsEngineAndFuel> engineList = carIdentification.findMatchingEngines(url, aztecData);
+
+			LOGGER.debug("engineList.size = " + engineList.size());
+
+			if (engineList.size() > 1) {
+				req.setAttribute("modelName", carFromApi.getCarsModel());
+				req.setAttribute("brandName", carFromApi.getCarsBrand());
+
+				formData.setCarBrand(carFromApi.getCarsBrand().getName());
+				formData.setCarModel(carFromApi.getCarsModel().getName());
+
+				req.setAttribute("engines", engineList);
+
+				RequestDispatcher dispatcher = req.getRequestDispatcher("CarEngineChoosingForm.jsp");
+				dispatcher.forward(req, resp);
+
+			} else if (engineList.size() == 1) {
+
+				String engineName = engineList.get(0).getName();
+				String engineLink = engineList.get(0).getLink();
+
+				req.setAttribute("engineName", engineList.get(0).getEngine());
+				req.setAttribute("modelName", carFromApi.getCarsModel());
+				req.setAttribute("brandName", carFromApi.getCarsBrand());
+
+				req.setAttribute("isFirstCat", true);
+
+				String engineOut = engineName + ";" + engineLink;
+
+				formData.setCarBrand(carFromApi.getCarsBrand().getName());
+				formData.setCarModel(carFromApi.getCarsModel().getName());
+				formData.setCarEngine(engineName);
+				formData.setEngineLookupString(engineOut);
+
+				url = "http://infoshareacademycom.2find.ru" + engineLink + "?lang=polish";
+
+				JsonDataAutopartCategories autopartCategories = parser.parseCategoryFile(url);
+				req.setAttribute("categories", autopartCategories.getData());
+
+				RequestDispatcher dispatcher = req.getRequestDispatcher("PartFirstCategoryChoosingForm.jsp");
+				dispatcher.forward(req, resp);
+			} else {
+				req.setAttribute("modelName", carFromApi.getCarsModel().getName());
+				req.setAttribute("brandName", carFromApi.getCarsBrand().getName());
+
+				formData.setCarBrand(carFromApi.getCarsBrand().getName());
+				formData.setCarModel(carFromApi.getCarsModel().getName());
+
+				DataCarsEngineAndFuel dataCarsEngineAndFuelModels = parser.parseEngineFile(url);
+				req.setAttribute("engines", dataCarsEngineAndFuelModels.getData());
+
+				RequestDispatcher dispatcher = req.getRequestDispatcher("CarEngineChoosingForm.jsp");
+				dispatcher.forward(req, resp);
+			}
+		}
+	}
+
+	private CarFromAztecData getCarFromApiString(String apiString) {
+		CarFromAztecData carFromApi = new CarFromAztecData();
+		carFromApi.setError("" + apiString.charAt(apiString.indexOf("\"Error\"") + 8));
+
+		LOGGER.info("Error Code: {}", carFromApi.getError());
+
+		if (carFromApi.getError().equals("0")) {
+
+			String[] apiStringArray = apiString.split(",\"");
+
+			for (String property : apiStringArray) {
+				if (property.contains("D1")) {
+					carFromApi.setCarBrand(getPropertyValue(property));
+					continue;
+				}
+				if (property.contains("D5")) {
+					carFromApi.setCarModel(getPropertyValue(property));
+					continue;
+				}
+				if (property.contains("Rok_produkcji")) {
+					carFromApi.setProductionYear(getPropertyValue(property));
+					continue;
+				}
+				if (property.contains("ErrorText")) {
+					carFromApi.setErrorText(getPropertyValue(property));
+					continue;
+				}
+				if (property.contains("P.1")) {
+					carFromApi.setEngineCapacity(getPropertyValue(property));
+					continue;
+				}
+				if (property.contains("P.2")) {
+					carFromApi.setEnginePower(getPropertyValue(property));
+					continue;
+				}
+				if (property.contains("P.3")) {
+					carFromApi.setFuelType(getPropertyValue(property));
+					continue;
+				}
+			}
+			return carFromApi;
+		} else {
+			return null;
+		}
+
+	}
+
+	private String getPropertyValue(String property) {
+		LOGGER.info("getPropertyValue({})", property);
+
+		String value = property.split(":")[1];
+
+		Pattern p = Pattern.compile("\\\".*?\\\"");
+		Matcher m = p.matcher(value);
+		if (m.find())
+			value = m.group().subSequence(1, m.group().length() - 1).toString();
+
+		LOGGER.info("Property {} value: {}", property,  value);
+		return value;
+	}
+}
