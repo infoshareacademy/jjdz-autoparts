3546adf99f6749e0a2640d6115b383ebf2b872e2
daniel-kepczynski <danielkepczynski@gmail.com>
2016-07-20 17:52:56 +0200

    Niedziałająca metoda do wysyłania maila

--- report-module/src/main/java/reports/trigger/ReportTrigger.java
+++ report-module/src/main/java/reports/trigger/ReportTrigger.java
@@ -1,29 +1,53 @@
 package reports.trigger;
 
+import reports.mailing.Email;
+import reports.mailing.EmailService;
 import reports.users.GetUsersEmail;
 
 import javax.ejb.EJB;
 import javax.ejb.Schedule;
 import javax.ejb.Startup;
 import javax.ejb.Stateless;
+import javax.mail.MessagingException;
 import java.io.IOException;
-import javax.ejb.Schedule;
-import javax.ejb.Startup;
-import javax.ejb.Stateless;
 
-@Stateless
-@Startup
+//@Stateless
+//@Startup
 public class ReportTrigger {
 
-    @EJB
-    GetUsersEmail getUsersEmail;
+//    private static final String SENDER_EMAIL = "napiwoda@op.pl";
+//
+////    @EJB
+////    static GetUsersEmail getUsersEmail;
+////
+////    @EJB
+////    static EmailService emailService;
+//
+//    @Schedule(second = "*/5",  hour = "*", minute = "*")
+//    public void run() throws IOException {
 
-    @Schedule(second = "*/5",  hour = "*", minute = "*")
-    public void run() throws IOException {
-        System.out.println("test");
-        String userEmails = getUsersEmail.getEmails();
+
+
+
+
+
+    //}
+
+    public static void main(String[] args) throws IOException, MessagingException {
+
+//        Email email = new Email();
+//
+//        EmailService emailService = new EmailService();
+//
+//        email.setReceiver("danielkepczynski@gmail.com");
+//        email.setTitle("Raport wyszukiwanych części");
+//        email.setSender("autoparts.reporting@gmail.com");
+//        email.setMessage("test");
+//        emailService.sendMail(email);
+
 
     }
 
 
 }
+
