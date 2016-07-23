f2348cd41c956fe4c2da6ad54dd0503e47184f55
daniel-kepczynski <danielkepczynski@gmail.com>
2016-07-23 17:10:00 +0200

    wysyłanie maila z raportem tygodniowym

--- report-module/src/main/java/reports/trigger/ReportTrigger.java
+++ report-module/src/main/java/reports/trigger/ReportTrigger.java
@@ -2,49 +2,59 @@ package reports.trigger;
 
 import reports.mailing.Email;
 import reports.mailing.EmailService;
+import reports.searched.part.MostFrequentlySearchedParts;
+import reports.searched.part.model.DTOwithSum;
 import reports.users.GetUsersEmail;
+import reports.users.User;
 
 import javax.ejb.EJB;
 import javax.ejb.Schedule;
 import javax.ejb.Startup;
 import javax.ejb.Stateless;
+import javax.jws.soap.SOAPBinding;
 import javax.mail.MessagingException;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-//@Stateless
-//@Startup
+@Stateless
+@Startup
 public class ReportTrigger {
 
-//    private static final String SENDER_EMAIL = "napiwoda@op.pl";
-//
-////    @EJB
-////    static GetUsersEmail getUsersEmail;
-////
-////    @EJB
-////    static EmailService emailService;
-//
-//    @Schedule(second = "*/5",  hour = "*", minute = "*")
-//    public void run() throws IOException {
 
+    public static final String MAIL_TITLE = "Raport tygodniowy wyszukiwanych części";
 
+    @EJB
+    GetUsersEmail getUsersEmail;
 
+    @EJB
+    EmailService emailService;
 
+    @EJB
+    MostFrequentlySearchedParts searchedParts;
 
+    @Schedule(second = "*/30", hour = "*", minute = "*")
+    public void run() throws IOException, MessagingException {
 
-    //}
+        ArrayList<User> users = getUsersEmail.getEmails();
 
-    public static void main(String[] args) throws IOException, MessagingException {
+        List<DTOwithSum> weeklyReport = searchedParts.getPartsForReport(LocalDateTime.now().minusDays(7l), LocalDateTime.now());
 
-//        Email email = new Email();
-//
-//        EmailService emailService = new EmailService();
-//
-//        email.setReceiver("danielkepczynski@gmail.com");
-//        email.setTitle("Raport wyszukiwanych części");
-//        email.setSender("autoparts.reporting@gmail.com");
-//        email.setMessage("test");
-//        emailService.sendMail(email);
+        String messageBody ="Witaj, \nponiżej znajduje się Twój raport tygodniowy:";
 
+        for (DTOwithSum withSum : weeklyReport) {
+            messageBody += withSum.toString();
+
+        }
+
+        for (User user : users) {
+            Email email = new Email();
+            email.setTitle(MAIL_TITLE);
+            email.setMessage(messageBody);
+            email.setReceiver(user.geteMail());
+            emailService.sendMail(email);
+        }
 
     }
 
